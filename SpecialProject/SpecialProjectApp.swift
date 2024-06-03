01857f71e755703638905c0b84829f2182ddaa9d
Elie BINGUE <eliedunamis7@icloud.com>
2024-06-03 11:33:06 +0200

    Initial Commit

---
+++ SpecialProject/SpecialProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpecialProjectApp.swift
+//  SpecialProject
+//
+//  Created by Apprenant 125 on 03/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct SpecialProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
